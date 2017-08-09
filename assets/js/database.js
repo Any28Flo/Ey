2f68f8ffc7269ad4eb8cceb07b72ceb55d9ab22a
Analin Flores <foglzerika@gmail.com>
2017-08-09 23:32:40 +0200

    Iniciando bd

---
+++ assets/js/database.js
@@ -0,0 +1,13 @@
+
+<script src="https://www.gstatic.com/firebasejs/4.2.0/firebase.js"></script>
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB9c-FFxeo0MEDb8U230jBWVcMFo9rUGXQ",
+    authDomain: "eydatabase.firebaseapp.com",
+    databaseURL: "https://eydatabase.firebaseio.com",
+    projectId: "eydatabase",
+    storageBucket: "eydatabase.appspot.com",
+    messagingSenderId: "584479342890"
+  };
+  firebase.initializeApp(config);
